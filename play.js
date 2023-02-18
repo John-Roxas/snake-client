77acac60fdd0ff9bbe26ca422c5bfe80e6d5cbd0
TheLoneliestTime <john.a.roxas@gmail.com>
2023-02-18 15:11:04 -0800

    set up two functions: setupInput and handleUserInput

--- play.js
+++ play.js
@@ -1,2 +1,23 @@
 const client = require('./client.js');
 client.connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+  
+
+  return stdin;
+};
+setupInput();
